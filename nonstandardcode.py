e0e2cfceccde7c13d0943037ff478fb09fdf1fc5
Farhana Asgar <farhana.asgar@tigeranalytics.local>
2024-09-20 15:31:05 +0530

    ENH: Redundant code removed

--- nonstandardcode.py
+++ nonstandardcode.py
@@ -47,12 +47,6 @@ housing["income_cat"] = pd.cut(housing["median_income"],
                                bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
                                labels=[1, 2, 3, 4, 5])
 
-housing["income_cat"] = pd.cut(
-    housing["median_income"],
-    bins=[0.0, 1.5, 3.0, 4.5, 6.0, np.inf],
-    labels=[1, 2, 3, 4, 5],
-)
-
 split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
 for train_index, test_index in split.split(housing, housing["income_cat"]):
     strat_train_set = housing.loc[train_index]
@@ -65,19 +59,6 @@ def income_cat_proportions(data):
 
 train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
 
-compare_props = pd.DataFrame(
-    {
-        "Overall": income_cat_proportions(housing),
-        "Stratified": income_cat_proportions(strat_test_set),
-        "Random": income_cat_proportions(test_set),
-    }
-).sort_index()
-compare_props["Rand. %error"] = (
-    100 * compare_props["Random"] / compare_props["Overall"] - 100
-)
-compare_props["Strat. %error"] = (
-    100 * compare_props["Stratified"] / compare_props["Overall"] - 100
-)
 compare_props = pd.DataFrame({
     "Overall": income_cat_proportions(housing),
     "Stratified": income_cat_proportions(strat_test_set),
@@ -97,20 +78,11 @@ housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1)
 
 # corr_matrix = housing.corr()
 # corr_matrix["median_house_value"].sort_values(ascending=False)
-housing["rooms_per_household"] = housing["total_rooms"] \
- / housing["households"]
-housing["bedrooms_per_room"] = housing["total_bedrooms"] \
- / housing["total_rooms"]
-housing["population_per_household"] = housing["population"] \
- / housing["households"]
 housing["rooms_per_household"] = housing["total_rooms"]/housing["households"]
 housing["bedrooms_per_room"] = housing["total_bedrooms"]/housing["total_rooms"]
 housing["population_per_household"] = housing["population"] \
     / housing["households"]
 
-housing = strat_train_set.drop(
-    "median_house_value", axis=1
-)  # drop labels for training set
 housing = strat_train_set.drop("median_house_value", axis=1)
 # drop labels for training set
 housing_labels = strat_train_set["median_house_value"].copy()
@@ -123,15 +95,6 @@ housing_num = housing.drop("ocean_proximity", axis=1)
 imputer.fit(housing_num)
 X = imputer.transform(housing_num)
 
-housing_tr = pd.DataFrame(X, columns=housing_num.columns, index=housing.index)
-housing_tr["rooms_per_household"] = housing_tr["total_rooms"] \
- / housing_tr["households"]
-housing_tr["bedrooms_per_room"] = (
-    housing_tr["total_bedrooms"] / housing_tr["total_rooms"]
-)
-housing_tr["population_per_household"] = (
-    housing_tr["population"] / housing_tr["households"]
-)
 housing_tr = pd.DataFrame(X, columns=housing_num.columns,
                           index=housing.index)
 housing_tr["rooms_per_household"] = housing_tr["total_rooms"] \
@@ -141,10 +104,6 @@ housing_tr["bedrooms_per_room"] = housing_tr["total_bedrooms"] \
 housing_tr["population_per_household"] = housing_tr["population"] \
     / housing_tr["households"]
 
-housing_cat = housing[["ocean_proximity"]]
-housing_prepared = housing_tr.join(
-    pd.get_dummies(housing_cat, drop_first=True)
-    )
 housing_cat = housing[['ocean_proximity']]
 housing_prepared = housing_tr.join(pd.get_dummies(
     housing_cat, drop_first=True))
@@ -229,10 +188,6 @@ X_test_cat = X_test[['ocean_proximity']]
 X_test_prepared = X_test_prepared.join(pd.get_dummies
                                        (X_test_cat, drop_first=True))
 
-X_test_cat = X_test[["ocean_proximity"]]
-X_test_prepared = X_test_prepared.join(
-    pd.get_dummies(X_test_cat, drop_first=True)
-    )
 
 final_predictions = final_model.predict(X_test_prepared)
 final_mse = mean_squared_error(y_test, final_predictions)
