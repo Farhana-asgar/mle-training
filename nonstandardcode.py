e62c6e58d2bab1f0e5c1546dcda186578550d3a3
Farhana Asgar <farhana.asgar@tigeranalytics.local>
2024-09-23 16:50:00 +0530

    Reverting changes

--- nonstandardcode.py
+++ nonstandardcode.py
@@ -1,8 +1,10 @@
+import os
+import tarfile
+
 import numpy as np
 import pandas as pd
-from data_ingestion import fetch_housing_data
-from data_loading import income_cat_proportions, load_housing_data
 from scipy.stats import randint
+from six.moves import urllib
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LinearRegression
@@ -15,7 +17,27 @@ from sklearn.model_selection import (
 )
 from sklearn.tree import DecisionTreeRegressor
 
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    os.makedirs(housing_path, exist_ok=True)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+
 fetch_housing_data()
+
+
+def load_housing_data(housing_path=HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(csv_path)
+
 
 housing = load_housing_data()
 
@@ -35,6 +57,12 @@ split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
 for train_index, test_index in split.split(housing, housing["income_cat"]):
     strat_train_set = housing.loc[train_index]
     strat_test_set = housing.loc[test_index]
+
+
+def income_cat_proportions(data):
+    return data["income_cat"].value_counts() / len(data)
+
+
 train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
 
 compare_props = pd.DataFrame(
