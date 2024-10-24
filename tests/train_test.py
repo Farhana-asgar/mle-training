dd95316745aa434eece28c5986a5e4859625c55b
Farhana Asgar <farhana.asgar@tigeranalytics.local>
2024-10-24 17:19:40 +0530

    ENH: MLFlow added

--- tests/train_test.py
+++ tests/train_test.py
@@ -1,9 +1,12 @@
+import os
+
 import numpy as np
 import pandas as pd
 import pytest
 import sklearn
 
-from house_value_prediction import train
+from house_value_prediction.ingest_data import IngestData
+from house_value_prediction.train import Train
 
 
 @pytest.fixture
@@ -19,13 +22,23 @@ def sample_data():
         'bedrooms_per_room': np.random.rand(25),
         'population_per_household': np.random.rand(25)
     })
+    temp_dir_dataset = './test_data'
+    temp_dir_model = './model_data'
+    os.makedirs(temp_dir_model, exist_ok=True)
+
+    # Initialize IngestData with the temp directory
+    ingest = IngestData(dataset_location=temp_dir_dataset)
+    train = Train(dataset_location=temp_dir_dataset,
+                  model_location=temp_dir_model)
+
+    # Clean up after tests
     housing_labels = pd.Series([1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5,
                                 1, 2, 3, 4, 5, 1, 2, 3, 4, 5])
-    return housing_prepared, housing_labels
+    return (housing_prepared, housing_labels, train)
 
 
 def test_lin_reg(sample_data):
-    housing_prepared, housing_labels = sample_data
+    housing_prepared, housing_labels, train = sample_data
     print(housing_prepared.shape)
     lin_reg_model = train.lin_reg(housing_prepared, housing_labels)
     print(type(lin_reg_model))
@@ -34,7 +47,7 @@ def test_lin_reg(sample_data):
 
 
 def test_dec_tree(sample_data):
-    housing_prepared, housing_labels = sample_data
+    housing_prepared, housing_labels, train = sample_data
     print(housing_prepared.shape)
     lin_reg_model = train.dec_tree(housing_prepared, housing_labels)
     print(type(lin_reg_model))
